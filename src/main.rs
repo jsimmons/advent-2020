ddd5b713c44b6bfbbb1b5b05bbe20317d53773a2
Josh Simmons <simmons44@gmail.com>
2020-12-01 10:06:13 +0100

    Solve Day 1, Part 2

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn read_numbers<R: Read>(io: R) -> Vec<i32> {
         .collect()
 }
 
-fn day_1(input: &[i32]) -> i32 {
+fn day_1_part_1(input: &[i32]) -> i32 {
     input
         .iter()
         .enumerate()
@@ -24,9 +24,30 @@ fn day_1(input: &[i32]) -> i32 {
         .unwrap()
 }
 
+fn day_1_part_2(input: &[i32]) -> i32 {
+    input
+        .iter()
+        .enumerate()
+        .find_map(|(i, x)| {
+            input[i..].iter().enumerate().find_map(|(j, y)| {
+                input[j..].iter().find_map(|z| {
+                    if x + y + z == 2020 {
+                        Some(x * y * z)
+                    } else {
+                        None
+                    }
+                })
+            })
+        })
+        .unwrap()
+}
+
 fn main() {
     let day_1_input =
         read_numbers(File::open("data/day_1.txt").expect("unable to find day 1 data"));
-    let day_1_result = day_1(&day_1_input);
-    println!("day 1 answer: {}", day_1_result);
+    let day_1_part_1_result = day_1_part_1(&day_1_input);
+    println!("day 1, part 1: {}", day_1_part_1_result);
+
+    let day_1_part_2_result = day_1_part_2(&day_1_input);
+    println!("day 1, part 2: {}", day_1_part_2_result);
 }

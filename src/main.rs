379ab4b3a286c82538621e38e461b1c8baa61c2f
Josh Simmons <simmons44@gmail.com>
2020-12-01 11:43:42 +0100

    Add instruction counters

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+#![cfg_attr(feature = "nightly", feature(asm))]
+
 use std::{
     fs::File,
     io::{BufRead, BufReader, Read},
 };
+
+use counters::Counter;
+
+pub mod counters;
 
 fn read_numbers<R: Read>(io: R) -> Vec<i32> {
     let buf_reader = BufReader::new(io);
@@ -45,11 +51,29 @@ fn day_1_part_2(input: &[i32]) -> i32 {
 fn main() {
     let day_1_input =
         read_numbers(File::open("data/day_1.txt").expect("unable to find day 1 data"));
+
+    let instruction_counter = Counter::by_name("instructions-minus-irqs:u").unwrap();
+
+    let start = instruction_counter.since_start();
     let day_1_part_1_result = day_1_part_1(&day_1_input);
-    println!("day 1, part 1: {}", day_1_part_1_result);
+    let end = instruction_counter.since_start();
+
+    println!(
+        "day 1, part 1\nanswer: {}\ninstuctions: {}",
+        day_1_part_1_result,
+        (end - start)
+    );
     assert_eq!(day_1_part_1_result, 889779);
 
+    println!();
+
+    let start = instruction_counter.since_start();
     let day_1_part_2_result = day_1_part_2(&day_1_input);
-    println!("day 1, part 2: {}", day_1_part_2_result);
+    let end = instruction_counter.since_start();
+    println!(
+        "day 1, part 2\nanswer: {}\ninstuctions: {}",
+        day_1_part_2_result,
+        (end - start)
+    );
     assert_eq!(day_1_part_2_result, 76110336);
 }
